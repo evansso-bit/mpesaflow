6a19047d109c1744a401ab747f6875ff14807b04
Evansso <marsapollo3@gmail.com>
2024-09-21 18:55:01 +0300

    Refactor welcome email logic into a dedicated function, simplifying RootLayout and improving email sending efficiency

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -1,19 +1,16 @@
-'use client'
 
 import "@mpesaflow/ui/globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
-import { redis } from "@mpesaflow/kv";
+
 import { Toaster } from '@mpesaflow/ui/sonner'
-import { toast } from "sonner";
+import { sendWelcomeEmail } from "../components/send-email";
+
 
 import { Inter } from 'next/font/google'
 
 import MainNav from "../components/main-nav";
 import { cn } from "@mpesaflow/ui/cn";
-import StripeWelcomeEmail from "../emails/welcome";
-import { useUser } from "@clerk/nextjs";
-import { Resend } from "resend";
-import { useEffect } from "react";
+
 
 const inter = Inter({
   display: 'swap',
@@ -21,14 +18,8 @@ const inter = Inter({
 })
 
 
-
-
-
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    sendWelcomeEmail();
-  }, []);
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  await sendWelcomeEmail();
   return (
     <ClerkProvider>
       <html lang="en">
@@ -45,36 +36,3 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 }
 
 
-async function sendWelcomeEmail() {
-  const resend = new Resend(process.env.RESEND_API_KEY);
-  const { isSignedIn, user } = useUser()
-  const primaryEmail = user?.primaryEmailAddress?.emailAddress;
-  const isAlreadySent = await redis.sismember('welcome-emails', primaryEmail);
-
-  if (isSignedIn) {
-    if (!isAlreadySent) {
-
-      try {
-
-        const { data, error } = await resend.emails.send({
-          from: 'onboarding@resend.dev',
-          to: `${primaryEmail}`,
-          subject: 'Welcome to MpesaFlow',
-          react: StripeWelcomeEmail(),
-        });
-
-
-        if (error) {
-          return Response.json({ error }, { status: 500 });
-          toast.error('Something went wrong. Please try again later.');
-        } else if (data) {
-          await redis.sadd('welcome-emails', primaryEmail);
-          toast.success('Welcome email sent successfully!');
-        }
-
-      } catch (error) {
-        return Response.json({ error }, { status: 500 })
-      }
-    }
-  }
-}
---
+++ apps/dashboard/src/components/send-email.tsx
@@ -0,0 +1,43 @@
+'use server'
+
+import { Resend } from "resend";
+import { useUser } from "@clerk/nextjs";
+import { toast } from "sonner";
+import { redis } from "@mpesaflow/kv";
+import { StripeWelcomeEmail } from "../emails/welcome";
+
+
+
+export async function sendWelcomeEmail() {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    const { isSignedIn, user } = useUser()
+    const primaryEmail = user?.primaryEmailAddress?.emailAddress;
+    const isAlreadySent = await redis.sismember('welcome-emails', primaryEmail);
+
+    if (isSignedIn) {
+        if (!isAlreadySent) {
+
+            try {
+
+                const { data, error } = await resend.emails.send({
+                    from: 'onboarding@resend.dev',
+                    to: `${primaryEmail}`,
+                    subject: 'Welcome to MpesaFlow',
+                    react: StripeWelcomeEmail(),
+                });
+
+
+                if (error) {
+                    return Response.json({ error }, { status: 500 });
+                    toast.error('Something went wrong. Please try again later.');
+                } else if (data) {
+                    await redis.sadd('welcome-emails', primaryEmail);
+                    toast.success('Welcome email sent successfully!');
+                }
+
+            } catch (error) {
+                return Response.json({ error }, { status: 500 })
+            }
+        }
+    }
+}
