1f3403834b620bc1ca0b34442300cebeff3af030
Evansso <marsapollo3@gmail.com>
2024-10-09 16:08:03 +0300

    fix

--- apps/dashboard/convex/apiActions.ts
+++ apps/dashboard/convex/apiActions.ts
@@ -2,91 +2,100 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 export const createApiKey = mutation({
-  args: {
-    key: v.string(),
-    userId: v.string(),
-    name: v.string(),
-    emailAdress: v.string(),
-    keyId: v.string(),
-    applicationId: v.string(),
-    enviroment: v.string(),
-  },
-  handler: async (ctx, args) => {
-    return await ctx.db.insert("apiKeys", args);
-  },
+	args: {
+		key: v.string(),
+		userId: v.string(),
+		name: v.string(),
+		emailAdress: v.string(),
+		keyId: v.string(),
+		applicationId: v.string(),
+		enviroment: v.string(),
+	},
+	handler: async (ctx, args) => {
+		return await ctx.db.insert("apiKeys", args);
+	},
 });
 
 export const getApiKeys = query({
-  args: {
-    userId: v.string(),
-    applicationId: v.string(),
-    enviroment: v.string(), // Note: Fixed typo in 'environment'
-  },
-  handler: async (ctx, args) => {
-    return await ctx.db
-      .query("apiKeys")
-      .filter((q) =>
-        q.and(
-          q.eq(q.field("userId"), args.userId),
-          q.eq(q.field("applicationId"), args.applicationId),
-          q.eq(q.field("enviroment"), args.enviroment),
-        ),
-      )
-      .collect();
-  },
+	args: {
+		userId: v.string(),
+		applicationId: v.string(),
+		enviroment: v.string(), // Note: Fixed typo in 'environment'
+	},
+	handler: async (ctx, args) => {
+		return await ctx.db
+			.query("apiKeys")
+			.filter((q) =>
+				q.and(
+					q.eq(q.field("userId"), args.userId),
+					q.eq(q.field("applicationId"), args.applicationId),
+					q.eq(q.field("enviroment"), args.enviroment)
+				)
+			)
+			.collect();
+	},
 });
 
 export const updateApiKey = mutation({
-  args: {
-    name: v.string(),
-    keyId: v.string(),
-    _id: v.string(),
-  },
-  handler: async (ctx, args) => {
-    const { _id } = args;
-    return await ctx.db.patch(_id, {
-      name: args.name,
-      keyId: args.keyId,
-    });
-  },
+	args: {
+		name: v.string(),
+		keyId: v.string(),
+		_id: v.optional(v.id("apiKeys")),
+	},
+	handler: async (ctx, args) => {
+		const { _id } = args;
+		if (!_id) {
+			return;
+		}
+		return await ctx.db.patch(_id, {
+			name: args.name,
+			keyId: args.keyId,
+		});
+	},
 });
 
 export const deleteApiKey = mutation({
-  args: {
-    _id: v.string(),
-  },
-  handler: async (ctx, args) => {
-    const { _id } = args;
-    return await ctx.db.delete(args._id);
-  },
+	args: {
+		_id: v.optional(v.id("apiKeys")),
+	},
+	handler: async (ctx, args) => {
+		const { _id } = args;
+		if (!_id) {
+			return;
+		}
+		const id = ctx.db.delete(_id);
+		return id;
+	},
 });
 
 export const getApiKeyDetails = query({
-  args: {
-    emailAdress: v.string(),
-  },
-  handler: async (ctx, args) => {
-    return await ctx.db
-      .query("apiKeys")
-      .filter((q) =>
-        q.and(
-          q.eq(q.field("userId"), args.userId),
-          q.eq(q.field("applicationId"), args.applicationId),
-          q.eq(q.field("enviroment"), args.enviroment),
-        ),
-      )
-      .first();
-  },
+	args: {
+		userId: v.string(),
+		applicationId: v.string(),
+		enviroment: v.string(),
+	},
+	handler: async (ctx, args) => {
+		return await ctx.db
+			.query("apiKeys")
+			.filter((q) =>
+				q.and(
+					q.eq(q.field("userId"), args.userId),
+					q.eq(q.field("applicationId"), args.applicationId),
+					q.eq(q.field("enviroment"), args.enviroment)
+				)
+			)
+			.first();
+	},
 });
 
 export const getApiKey = query({
-  args: {
-    applicationId: v.string(),
-  },
-  handler: async (ctx, args) => {
-    return await ctx.db
-      .query("apiKeys")
-      .filter((q) => q.eq(q.field("apiKeys"), args.applicationId))
-      .first();
-  },
+	args: {
+		applicationId: v.string(),
+	},
+	handler: async (ctx, args) => {
+		return await ctx.db
+			.query("apiKeys")
+			.filter((q) => q.eq(q.field("applicationId"), args.applicationId))
+			.first();
+	},
 });
