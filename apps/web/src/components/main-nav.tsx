37a1de4ab6ee2742c9218c78ecb49a756495f704
Evansso <marsapollo3@gmail.com>
2024-10-09 21:27:53 +0300

    Update package name to @mpesaflow/web and refactor layout structure for cleaner integration of metadata and components

--- apps/web/src/components/main-nav.tsx
+++ apps/web/src/components/main-nav.tsx
@@ -4,12 +4,14 @@ import Link from "next/link";
 
 export default function MainNav() {
   return (
-    <nav className="max-w-4xl justify-between border border-gray-200 w-full py-3 px-4 flex flex-row rounded-xl">
+    <nav className="max-w-4xl justify-between border border-gray-200 w-full py-2 px-4 flex flex-row rounded-xl mx-auto mt-10 bg-white items-center">
       <h1 className="text-lg">MpesaFlow</h1>
 
       <div>
         <SignedOut>
+        <Button>
           <SignInButton />
+          </Button>
         </SignedOut>
         <SignedIn>
           <Button>
